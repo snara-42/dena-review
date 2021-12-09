ca84433c52ce29b7d8a8008d3dfef3243a6cbafd
subaru nara <snara@student.42tokyo.jp>
2021-12-10 01:28:45 +0900

    fix

--- main.py
+++ main.py
@@ -2,25 +2,23 @@ import re
 import argparse
 
 
-
-
 class Game:
-    def __init__(self, d: dict):
-        self.x = d.get("x", 7)
-        self.y = d.get("y", 6)
-        self.len = d.get("len", 4)
-        self.sym = [d.get("e", "."), d.get("p1", "x"), d.get("p2", "o"), ]
+    def __init__(self, **kw):
+        self.x = kw.get("x", 7)
+        self.y = kw.get("y", 6)
+        self.len = kw.get("len", 4)
+        self.sym = [kw.get("e", "."), kw.get("p1", "x"), kw.get("p2", "o"), ]
+        self.logfile = kw.get("o", "result.log")
         self.board = ["" for _ in range(self.x)]
 
     def __str__(self):
-        return "\n".join((*[" ".join(s).translate(str.maketrans({k: v for k, v in zip(".xo", self.sym)})) for s in zip(*[col.ljust(self.y, ".")[::-1] for col in self.board])], self.sym[0].join([str(i) for i in range(1, self.x+1)])))
+        return "\n".join((*[" ".join(s).translate(str.maketrans({k: v for k, v in zip(".xo", self.sym)})) for s in zip(*[col.ljust(self.y, ".")[::-1] for col in self.board])], self.sym[0].join([str(i) for i in range(1, self.x+1)])))+"\n"
 
     @staticmethod
     def input_int(prompt):
         while True:
             try:
-                n = int(input(prompt).strip())
-                return n
+                return int(input(prompt).strip())
             except ValueError:
                 continue
 
@@ -36,21 +34,27 @@ class Game:
             + "|(("+c+".{"+str(self.y-1)+"}){"+str(self.len-1)+"}"+c+")"
             + "|(("+c+".{"+str(self.y) + "}){"+str(self.len-1)+"}"+c+")"
             + "|(("+c+".{"+str(self.y+1)+"}){"+str(self.len-1)+"}"+c+")")
-        print(reg)
         return reg.search("/".join(c.ljust(self.y, ".") for c in self.board))
 
     def play_round(self):
         for i in range(self.x * self.y):
-            print(self, f"\n{i+1} player{i%2+1}[{self.sym[i%2+1]}]'s turn")
+            print(f"\n{self}{i+1} player{i%2+1}[{self.sym[i%2+1]}]'s turn")
             n = self.input_valid_index("> ")
             c = "xo"[i % 2]
             self.board[n-1] += c
             if self.has_won(c):
-                print(self, f"\nplayer{i%2+1}[{self.sym[i%2+1]}] wins!\n\n")
+                res = f"\n{self}\nplayer{i%2+1}[{self.sym[i%2+1]}] wins!\n"
+                print(res)
                 break
         else:
-            print(self, "\ndraw!\n\n")
-        input("\nPress enter to play again ")
+            res = f"\n{self}\ndraw!\n"
+            print(res)
+        try:
+            with open(self.logfile, "a") as f:
+                f.write(res)
+        except Exception as e:
+            print(e)
+        input("\nPress enter to play again... ")
 
     def play(self):
         try:
@@ -75,9 +79,12 @@ def main():
                         help="board height")
     parser.add_argument("-len", "-length", type=int, default=4,
                         help="length to win")
+    parser.add_argument("-o", "-logfile", type=str, default="./result.log",
+                        metavar="FILE", help="file to save results")
     args = parser.parse_args()
 
-    game = Game(vars(args))
+    print(args)
+    game = Game(**vars(args))
     game.play()
 
 
